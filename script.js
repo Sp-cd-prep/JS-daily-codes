c5c68fb404406add8a51603f5115e258dc75da8f
SP Acharya <sthitaprangya.acharya@collegedekho.com>
2024-05-11 16:07:54 +0530

    conditional statement

---
+++ script.js
@@ -0,0 +1,155 @@
+
+
+// Q1. Find if the conditions are obeyed or not?
+// You are given two number first as A and second as B and check if both conditions (A<10 and A>B) are satisfied or not with the help of operators.
+// Note: You have to complete Is_Valid function. No need to take any input.
+
+// let A=5
+// let B=3
+
+// console.log((A<10 && A>B ))
+
+
+
+// Q2. You are given two numbers A and B. You need to do the following checks:
+// if both are divisible by 10 console true.
+// if both are not divisible by 10 console false.
+// if one of them only is divisible by 10 console true.
+// Use operator to do this.
+
+
+// let A=12
+// let B=20
+
+
+// if((A%10===0)&&(B%10===0)){
+//     console.log("true")
+// }
+
+// else if((A%10!==0)&&(B%10!==0)){
+//     console.log("false")
+// }
+
+// else if((A%10===0) || (B%10===0)){
+//     console.log("true on 3rd condition")
+// }
+
+
+// Q2. Find out the first and last digit of a 4 digit number 
+
+// let abc=1234;
+
+// console.log(parseInt(abc/1000)); //first digit
+// console.log(1234%10) //last digit
+
+
+// let a=50;
+// let b=20;
+// let c=100;
+// a=a+b+c;
+// console.log(a);
+// avg=a/3; //56.666664
+// let final = avg.toFixed(2);
+// console.log(final)
+
+
+// Q5. find out the grades according to the obtained marks. 
+
+// 90 to 100 - "O"
+// 80 to 90 - "A"
+// 70 to 80 - "B"
+// below 70 - "C"
+
+// find out the grade
+// let marks = 97;
+
+// if(marks>=90 && marks<=100){
+//     console.log("O")
+// }
+// else if(marks>=80 && marks<=90){
+//     console.log("A")
+// }
+// else if(marks>=70 && marks<=80){
+//     console.log("B")
+// }
+// else {
+//     console.log("C")
+// }
+
+// Switch case statement
+
+// Some students obtained the grades. Create a report to generate the approximate marks a students contain according to the grade. 
+
+// O - 90 to 100
+// A - 80 to 90
+// B - 70 to 80
+// C - below 70
+// default- failed
+
+
+
+// let grade = "B";
+// let marks;
+
+// switch(grade){
+//     case "O":
+//         marks = "90 to 100";
+//         break;
+//     case "A":
+//         marks = "80 to 90";
+//         break; 
+//     case "B":
+//         marks = "70 to 80";
+//         break;
+//     case "C":
+//         marks = "below 70";
+//         break;        
+//     default:
+//         marks = "failed";      
+// }
+// console.log("marks obtain:",marks)
+
+// console.log("out of the switch block ")
+
+
+
+// Q7. find out a number is even or not
+
+// let even = 13;
+// if(even % 2 === 0){
+//     console.log("Number is even")
+// }
+// else{
+//     console.log("Number is odd")
+// }
+
+// Q8. find out the greater number outof 2 numbrs
+
+
+
+
+// Q9. Find out the largest number out of 3 numbers?
+// Q10. Write a code to swap 2 numbers?
+// Q11. Find out the second smallest number out of 3 numbers?
+
+
+// Q10. swap 2 numbers
+
+// var a=8;
+// var b=18;
+// let c;
+// c=a;
+// a=b;
+// b=c;
+
+// without using 3rd variable
+// a=a+b;
+// b=a-b;
+// a=a-b;
+
+// console.log(a,b)
+
+
+
+
+
