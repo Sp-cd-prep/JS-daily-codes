22a9a0c6d2698edf97b4d33111322359280ccb9f
SP Acharya <sthitaprangya.acharya@collegedekho.com>
2024-05-30 16:53:46 +0530

    array

---
+++ array.js
@@ -0,0 +1,105 @@
+let arr = [1,2,3,4,5];
+
+// console.log(arr[0])  //1
+// console.log(arr[2])  //3
+
+// console.log(arr.length) //5
+// console.log(arr.length-1)//4
+// //index = arr.length-1
+
+// console.log(arr[arr.length-1]) //5
+// console.log(arr[(arr.length)*2-7]) //4
+// console.log(arr[(arr.length-1)/2+1]) //4
+
+
+// Q1.Iterate all the elements in an array
+
+// for(i=0;i<arr.length;i++){
+//     console.log(arr[i]);  //1 2 3 4 5 
+// }
+
+// Q2. Add 2 in each element
+// for(i=0;i<arr.length;i++){
+//     console.log(arr[i]+2);
+// }
+
+// Q3. Find out the even numbers 
+// for(i=0;i<arr.length;i++){
+//     if(arr[i]%2===0){
+//         console.log(arr[i]);
+//     }
+// }
+
+// Q4. Find out the odd number and add 3 to each
+
+// Q5. Find out the maximum number 
+
+// let arr = [4,1,7,2,8,3]
+
+// let max=arr[0]
+// for(i=0;i<arr.length;i++){
+//     if(arr[i]>max){
+//         max=arr[i]
+//     }
+// }
+// console.log(max)
+
+// Q6. Swap 2 elements
+
+// for(i=0;i<arr.length;i++){
+//     let temp = arr[1];
+//     arr[1]=arr[4];
+//     arr[4]=temp;
+// }
+// console.log(arr)
+
+// Q7. Reverse the array
+
+// let arr=[1,2,3,4,5]
+// let s=0;
+// let e=arr.length-1
+// let temp;
+
+// while(s<e){
+//     temp = arr[s];
+//     arr[s]=arr[e];
+//     arr[e]=temp;
+//     s++;
+//     e--;
+// }
+// console.log(arr)
+
+// Q8. Sort the array
+
+
+// let arr = [2,1,7,3,1,2]
+// let temp=0;
+// for(i=0;i<arr.length;i++){
+//     for(j=i+1;j<arr.length;j++){
+//         if(arr[i]>arr[j]){
+//             temp = arr[i];
+//             arr[i]=arr[j];
+//             arr[j]=temp;
+//         }
+//     }
+// }
+//     console.log(arr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
